f998f204fed44ca548376cbb840b645aa9f2600e
Trinal0821 <find@A5-004030.localdomain>
2018-11-07 17:16:42 -0700

    introjs

---
+++ app.js
@@ -0,0 +1,105 @@
+var sum = 1 + 2
+
+ //alert (sum)
+ /*
+ Multi 
+ line 
+ comment
+ */
+var diff = 1 - 2
+var product = 1 * 2 
+var div = 1 / 2
+
+// order of operation
+console.log( 1 + 2 * 3 + (9-2))
+//console.log(1 - 2)
+console.log(diff)
+console.log(product)
+console.log(div)
+
+var string = "Hello"
+var string2 = "world"
+
+// "Hello" + "world" equals "Hello world"
+// adding two strings, term is concatination
+console.log(string + string2)
+
+//consule.log(string + 1)
+// = assignment
+// == equality 
+
+
+//integer whole number 1, 3, 17
+
+// float decimal numbers 1.2, 3.14, 12.0
+
+// boolean true, false       1 0 
+
+// arrays collection of objects
+// []
+// [1, 4, 5, 2]
+// ["hi", "hello", "hey"]
+// [ 1, "hi", 1.3, true]
+
+
+// Function
+sayHello()
+
+function sayHello() {
+    console.log('hello')
+}
+//sayHello()
+
+var num1 = 2
+var num2 = 12
+
+function product(x, y) 
+    console.log(x * y)
+}
+
+
+product(num1, num2)
+
+function btnClick() {
+    alert('Yay you clicked me')
+}
+
+function mouseOver() {
+    console.log('hi')
+}
+
+var h = 3
+//reassignment h is now 11
+h = 1
+h = 7 
+h = 11
+
+l = 1
+
+//console.log(l)
+//conditions
+//console.log(1 == true)
+console.log("hello" == 1)
+//console.log("1 == 1")
+// array
+//var  arr = [1, 2, 3, 4, 5]
+// index    0  1  2  3  4
+// possition of arrays are called indexes, indexes are 0 based 
+
+//</script>console.log(arr[1])
+//console.log(arr[2])
+
+//var my_arr = [1, "hi", 1.2, true, 23]
+//console.log(myArr[3])
+
+//var mySuperArr = [ [1,3], ['ae', 'we'], [1.23, true] ]
+//console.log(mySuperArr[2] [1])
+
+//console.log(arr.shift())
+//console.log(arr.length)
+
+//add item onto array
+console.log(arr.push(2))
+
+//remove last element from array
+console.log(arr.pop())
